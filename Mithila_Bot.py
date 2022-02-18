6b964ba2ef5867e97f4db36697d8197abbb35752
Nitish Jha <nitishkumar0914@gmail.com>
2022-02-19 01:47:39 +0530

    Initial trial

---
+++ Mithila_Bot.py
@@ -0,0 +1,32 @@
+from ast import keyword
+from numpy import result_type
+import tweepy
+import keys
+import time, datetime
+
+auth = tweepy.OAuthHandler(keys.API_KEY, keys.API_SECRET_KEY)
+auth.set_access_token(keys.ACCESS_TOKEN, keys.SECRET_ACCESS_TOKEN)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+def twitter_bot(Keywords, delay):
+  while True:
+    print(f"\n{datetime.datetime.now()}\n")
+
+    for tweets in tweepy.Cursor(api.search_tweets, q=Keywords, result_type="mixed", count=5).items(5):
+      try:
+        tweet_id = dict(tweets._json)["id"]
+        tweet_text = dict(tweets._json)["text"]
+
+        print("id: " + str(tweet_id))
+        print("text: " + str(tweet_text))
+
+        api.retweet(tweet_id)
+
+      except:
+        print("Error")
+
+    time.sleep(delay)
+
+twitter_bot("मिथिला", 301)
+
+
